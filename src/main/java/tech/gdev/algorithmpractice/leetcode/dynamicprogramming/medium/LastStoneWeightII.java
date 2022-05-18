eba5fe9ca37f7ab13cd8c1695703a12451dac0a4
gdev <gdeveloping0@gmail.com>
2022-05-18 22:26:14 +0800

    refactor 1049. Medium LastStoneWeightII

--- src/main/java/tech/gdev/algorithmpractice/leetcode/dynamicprogramming/medium/LastStoneWeightII.java
+++ src/main/java/tech/gdev/algorithmpractice/leetcode/dynamicprogramming/medium/LastStoneWeightII.java
@@ -14,7 +14,7 @@ public class LastStoneWeightII {
     public static void main(String[] args) {
         Solution1 solution1 = new Solution1();
         int[] stones = new int[]{2,7,4,1,8,1};
-        System.out.println(Arrays.toString(stones));
+        System.out.println("res: " + Arrays.toString(stones));
         int res = solution1.lastStoneWeightII(stones);
         int output = 1;
         System.out.println("res: " + res + "\toutput: " + (res == output));
@@ -23,7 +23,7 @@ public class LastStoneWeightII {
     private static class Solution1 {
         /**
          * time: O(MN), N:len(stones), M:sum(stones)/2
-         * space: O(N)
+         * space: O(M)
          *
          * Runtime: 4 ms, faster than 64.09% of Java online submissions for Last Stone Weight II.
          * Memory Usage: 41.5 MB, less than 70.60% of Java online submissions for Last Stone Weight II.
@@ -40,10 +40,8 @@ public class LastStoneWeightII {
             int half = sum / 2;
             int[] dp = new int[half+1];
             for (int i = 1; i <= size; i++) {
-                for (int j = half; j > 0; j--) {
-                    if (j >= stones[i-1]) {
-                        dp[j] = Math.max(dp[j], dp[j-stones[i-1]] + stones[i-1]);
-                    }
+                for (int j = half; j >= stones[i-1]; j--) {
+                    dp[j] = Math.max(dp[j], dp[j-stones[i-1]] + stones[i-1]);
                 }
 //                System.out.println(Arrays.toString(dp));
             }
