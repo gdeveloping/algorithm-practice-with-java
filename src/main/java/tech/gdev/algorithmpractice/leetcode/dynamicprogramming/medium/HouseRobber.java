208eeebdc3b3ac538b70fa19275ea81884142774
gdev <gdeveloping0@gmail.com>
2022-05-21 21:14:47 +0800

    optimize 198. Medium HouseRobber

--- src/main/java/tech/gdev/algorithmpractice/leetcode/dynamicprogramming/medium/HouseRobber.java
+++ src/main/java/tech/gdev/algorithmpractice/leetcode/dynamicprogramming/medium/HouseRobber.java
@@ -1,5 +1,8 @@
 package tech.gdev.algorithmpractice.leetcode.dynamicprogramming.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 198.
  * Medium
@@ -53,6 +56,66 @@ public class HouseRobber {
                 dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
             }
             return dp[size-1];
+        }
+    }
+
+    private static class Solution3_1 {
+        /**
+         * time: O(N)
+         * space: O(N)
+         *
+         * Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber.
+         * Memory Usage: 39.8 MB, less than 79.95% of Java online submissions for House Robber.
+         *
+         * @param nums -
+         * @return -
+         */
+        public int rob(int[] nums) {
+            if (nums.length == 1) {
+                return nums[0];
+            }
+            memo = new HashMap<>();
+            return helper(nums, 0);
+        }
+
+        Map<Integer, Integer> memo;
+
+        private int helper(int[] nums, int start) {
+            if (start >= nums.length) {
+                return 0;
+            }
+            if (memo.containsKey(start)) {
+                return memo.get(start);
+            }
+            int res =  Math.max(
+                    helper(nums, start+1), // not rob
+                    nums[start] + helper(nums, start+2) // rob
+            );
+            memo.put(start, res);
+            return res;
+        }
+    }
+
+    private static class Solution3_2 {
+        /**
+         * time: O(N)
+         * space: O(N)
+         *
+         * Runtime: 0 ms, faster than 100.00% of Java online submissions for House Robber.
+         * Memory Usage: 41.3 MB, less than 57.47% of Java online submissions for House Robber.
+         *
+         * @param nums -
+         * @return -
+         */
+        public int rob(int[] nums) {
+            if (nums.length == 1) {
+                return nums[0];
+            }
+            int[] dp = new int[nums.length+2];
+            for (int i = nums.length - 1; i >= 0; i--) {
+                dp[i] = Math.max(dp[i+1], nums[i] + dp[i+2]);
+            }
+            return dp[0];
         }
     }
 }
