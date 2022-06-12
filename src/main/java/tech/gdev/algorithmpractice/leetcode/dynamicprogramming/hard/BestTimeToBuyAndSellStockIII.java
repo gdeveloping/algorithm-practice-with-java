666638a76ebd1f074bf4af09d6f484e30377c22c
gdev <gdeveloping0@gmail.com>
2022-06-12 19:16:56 +0800

    optimize 123. Hard BestTimeToBuyAndSellStockIII

--- src/main/java/tech/gdev/algorithmpractice/leetcode/dynamicprogramming/hard/BestTimeToBuyAndSellStockIII.java
+++ src/main/java/tech/gdev/algorithmpractice/leetcode/dynamicprogramming/hard/BestTimeToBuyAndSellStockIII.java
@@ -10,9 +10,9 @@ package tech.gdev.algorithmpractice.leetcode.dynamicprogramming.hard;
 public class BestTimeToBuyAndSellStockIII {
 
     public static void main(String[] args) {
-        Solution1 solution1 = new Solution1();
+        Solution1 s1 = new Solution1();
         int[] prices = new int[]{3,3,5,0,0,3,1,4};
-        int res = solution1.maxProfit(prices);
+        int res = s1.maxProfit(prices);
         System.out.println(res);
     }
 
@@ -40,6 +40,34 @@ public class BestTimeToBuyAndSellStockIII {
                 twoBuyTwoSell = Math.max(twoBuyTwoSell, p - twoBuy);
             }
             return twoBuyTwoSell;
+        }
+    }
+
+    private static class Solution2 {
+        /**
+         * time: O(N)
+         * space: O(1)
+         *
+         * Runtime: 9 ms, faster than 40.03% of Java online submissions for Best Time to Buy and Sell Stock III.
+         * Memory Usage: 78.2 MB, less than 57.44% of Java online submissions for Best Time to Buy and Sell Stock III.
+         *
+         * action: [buy, sell, rest]
+         * status: dp[i][max k transactions][if hold]
+         * k = 2 --> i10, i11, i20, i21
+         *
+         * @param prices -
+         * @return -
+         */
+        public int maxProfit(int[] prices) {
+            int i10 = 0, i11 = Integer.MIN_VALUE;
+            int i20 = 0, i21 = Integer.MIN_VALUE;
+            for (int price : prices) {
+                i20 = Math.max(i20, i21 + price);
+                i21 = Math.max(i21, i10 - price);
+                i10 = Math.max(i10, i11 + price);
+                i11 = Math.max(i11, -price);
+            }
+            return i20;
         }
     }
 }
