6ab7bcdb566c44d6da189ae9ac3d0a58db44ac14
gdev <gdeveloping0@gmail.com>
2022-06-26 22:51:43 +0800

    optimize 621. Medium TaskScheduler

--- src/main/java/tech/gdev/algorithmpractice/leetcode/greedy/medium/TaskScheduler.java
+++ src/main/java/tech/gdev/algorithmpractice/leetcode/greedy/medium/TaskScheduler.java
@@ -15,6 +15,7 @@ public class TaskScheduler {
         /**
          * time: O(N)
          * space: O(1)
+         * modify source: N
          *
          * Runtime: 6 ms, faster than 58.44% of Java online submissions for Task Scheduler.
          * Memory Usage: 60.1 MB, less than 46.63% of Java online submissions for Task Scheduler.
@@ -55,6 +56,10 @@ public class TaskScheduler {
         }
 
         /**
+         * time: O(N)
+         * space: O(N)
+         * modify source: N
+         *
          * Runtime: 53 ms, faster than 26.53% of Java online submissions for Task Scheduler.
          * Memory Usage: 67.4 MB, less than 29.08% of Java online submissions for Task Scheduler.
          *
@@ -87,6 +92,59 @@ public class TaskScheduler {
                 }
             }
             return count;
+        }
+    }
+
+    private static class Solution3 {
+        /**
+         * time: O(N)
+         * space: O(1)
+         * modify source: N
+         *
+         * Runtime: 94 ms, faster than 11.28% of Java online submissions for Task Scheduler.
+         * Memory Usage: 67.5 MB, less than 34.16% of Java online submissions for Task Scheduler.
+         *
+         * @param tasks -
+         * @param n -
+         * @return -
+         */
+        public int leastInterval(char[] tasks, int n) {
+            Map<Character, Integer> freq = new HashMap<>(); // size: 26
+            for (char ch : tasks) {
+                freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+            }
+
+            int size = freq.size();
+            List<Integer> nextValid = new ArrayList<>();
+            List<Integer> rest = new ArrayList<>();
+            for (Map.Entry<Character, Integer> entry : freq.entrySet()) {
+                nextValid.add(1);
+                rest.add(entry.getValue());
+            }
+
+            int time = 0;
+            for (int i = 0; i < tasks.length; ++i) {
+                ++time;
+                int minNextValid = Integer.MAX_VALUE;
+                for (int j = 0; j < size; ++j) {
+                    if (rest.get(j) != 0) {
+                        minNextValid = Math.min(minNextValid, nextValid.get(j));
+                    }
+                }
+                time = Math.max(time, minNextValid);
+                int best = -1;
+                for (int j = 0; j < size; ++j) {
+                    if (rest.get(j) != 0 && nextValid.get(j) <= time) {
+                        if (best == -1 || rest.get(j) > rest.get(best)) {
+                            best = j;
+                        }
+                    }
+                }
+                nextValid.set(best, time + n + 1);
+                rest.set(best, rest.get(best) - 1);
+            }
+
+            return time;
         }
     }
 }
