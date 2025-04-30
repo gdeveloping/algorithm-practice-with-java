c61885d9610d7469bf118e2ddb5091be62065fab
gdev <gdeveloping0@gmail.com>
2025-04-30 23:35:41 +0800

    23. Hard MergeKSortedLists

--- src/main/java/tech/gdev/algorithmpractice/leetcode/heap/hard/MergeKSortedLists.java
+++ src/main/java/tech/gdev/algorithmpractice/leetcode/heap/hard/MergeKSortedLists.java
@@ -1,74 +1,81 @@
 package tech.gdev.algorithmpractice.leetcode.heap.hard;
 
 import tech.gdev.algorithmpractice.leetcode.linkedlist.single.ListNode;
+import tech.gdev.algorithmpractice.utils.ListNodeUtil;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.PriorityQueue;
 
 /**
  * 23.
  * Hard
+ * Merge K Sorted Lists
  *
  * @author gdev
  * @date 2022/4/24 21:19
  */
 public class MergeKSortedLists {
     public static void main(String[] args) {
-        int[][] nums;
-        nums = new int[][]{{1,4,5},{1,3,4},{2,6}};
-        ListNode[] lists = new ListNode[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            ListNode dummy = new ListNode();
-            ListNode tail = dummy;
-            for (int j = 0; j < nums[i].length; j++) {
-                tail.next = new ListNode(nums[i][j]);
-                tail = tail.next;
-            }
-            lists[i] = dummy.next;
-        }
-        for (int i = 0; i < lists.length; i++) {
-            ListNode node = lists[i];
-            while (node != null) {
-                System.out.printf("%3d", node.val);
-                node = node.next;
-            }
-            System.out.println();
-        }
-        Solution2 solution2 = new Solution2();
-        ListNode node = solution2.mergeKLists(lists);
-        while (node != null) {
-            System.out.printf("%3d", node.val);
-            node = node.next;
-        }
+        int[][] nums1 = new int[][]{{1,4,5},{1,3,4},{2,6}};
+        testCaseSolution2(nums1);
+        int[][] nums2 = new int[][]{};
+        testCaseSolution2(nums2);
+        int[][] nums3 = new int[][]{{}};
+        testCaseSolution2(nums3);
+    }
+
+    private static void testCaseSolution1(int[][] nums) {
+        ListNode[] arr = ListNodeUtil.convertArraysToListNode(nums);
+        System.out.println("\nSolution1");
+        System.out.println("Source: " + Arrays.deepToString(nums));
+        ListNode merged = new Solution1().mergeKLists(arr);
+        System.out.println("Merged: ");
+        ListNodeUtil.printListNode(merged);
+    }
+
+    private static void testCaseSolution2(int[][] nums) {
+        ListNode[] arr = ListNodeUtil.convertArraysToListNode(nums);
+        System.out.println("\nSolution2");
+        System.out.println("Source: " + Arrays.deepToString(nums));
+        ListNode merged = new Solution2().mergeKLists(arr);
+        System.out.println("Merged: ");
+        ListNodeUtil.printListNode(merged);
     }
 
     private static class Solution1 {
         /**
-         * time: O(log(K)N)
+         * time: O(N*log(K))
          * space: O(K)
          *
-         * Runtime: 5 ms, faster than 73.01% of Java online submissions for Merge k Sorted Lists.
-         * Memory Usage: 47.5 MB, less than 43.01% of Java online submissions for Merge k Sorted Lists.
-         *
-         * @param lists -
-         * @return -
+         * time: 6ms 29.04%
+         * space: 43.41MB 97.65%
          */
         public ListNode mergeKLists(ListNode[] lists) {
-            if (lists == null || lists.length < 1) {return null;}
-            PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, (a,b) -> a.val - b.val);
+            if (lists == null || lists.length < 1) {
+                return null;
+            }
+            if (lists.length == 1) {
+                return lists[0];
+            }
             ListNode dummy = new ListNode();
-            ListNode tail = dummy;
-            for (ListNode node : lists) {
+            ListNode curr = dummy;
+            PriorityQueue<ListNode> pq = new PriorityQueue<>(lists.length, Comparator.comparingInt(a -> a.val));
+            for (ListNode pItem : lists) {
+                if (pItem != null) {
+                    pq.offer(pItem);
+                }
+            }
+            while (pq.size() > 1) {
+                ListNode node = pq.poll();
+                curr.next = node;
+                curr = curr.next;
+                node = node.next;
                 if (node != null) {
-                    queue.offer(node);
+                    pq.offer(node);
                 }
             }
-            while (!queue.isEmpty()) {
-                tail.next = queue.poll();
-                tail = tail.next;
-                if (tail.next != null) {
-                    queue.offer(tail.next);
-                }
-            }
+            curr.next = pq.poll();
             return dummy.next;
         }
     }
@@ -78,19 +85,23 @@ public class MergeKSortedLists {
          * time: O(log(K)N)
          * space: O(N)
          *
-         * Runtime: 1 ms, faster than 100.00% of Java online submissions for Merge k Sorted Lists.
-         * Memory Usage: 44.6 MB, less than 75.65% of Java online submissions for Merge k Sorted Lists.
-         *
-         * @param lists -
-         * @return -
+         * time: 1ms 100.00%
+         * space: 43.43MB 94.95%
          */
         public ListNode mergeKLists(ListNode[] lists) {
-            if (lists == null || lists.length < 1) {return null;}
+            if (lists == null || lists.length < 1) {
+                return null;
+            }
+            if (lists.length == 1) {
+                return lists[0];
+            }
             return mergeKLists(lists, 0, lists.length-1);
         }
 
         private ListNode mergeKLists(ListNode[] lists, int left, int right) {
-            if (left == right) {return lists[left];}
+            if (left == right) {
+                return lists[left];
+            }
             int mid = (left + right) / 2;
             ListNode l1 = mergeKLists(lists, left, mid);
             ListNode l2 = mergeKLists(lists, mid+1, right);
